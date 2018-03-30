ead1fc9abe96568c4a0d3a8bce509e85adf3281a
toto <toto@titi.nc>
2018-03-30 15:23:12 +1100

    add map pins and list

--- tp-react-native/App.js
+++ tp-react-native/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TabNavigator } from 'react-navigation';
+import ListScreen from './ListScreen';
+import MapScreen from './MapScreen';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+export default TabNavigator({
+  List: { screen: ListScreen },
+  Map: { screen: MapScreen },
 });
